023e011c7d6bdbc150c4b3a39271b604d5f355a1
s204092 <s204092@student.dtu.dk>
2024-12-27 11:14:02 +0100

    Updated rules

---
+++ Frontend/src/scenes/Rules/PossibleRoles.tsx
@@ -0,0 +1,189 @@
+import { Box, useTheme } from "@mui/material";
+import { RollWithName } from "../../utils/diceUtils";
+import { tokens } from "../../theme";
+
+interface LangProps {
+  isDanish: boolean;
+}
+
+export const POSSIBLEROLLS = ({ isDanish }: LangProps) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box display="flex" justifyContent="center" flexDirection="column">
+      <Box display="flex" justifyContent="center">
+        {isDanish
+          ? "Her er alle de slag, man kan rulle, fra højest til lavest:"
+          : "Here are all the possible rolls sorted  from highest to lowest:"}
+      </Box>
+      <Box p={1} />
+      {/* SPECIAL */}
+      <Box display="flex" justifyContent="center">
+        {isDanish
+          ? "Fællesskålsslaget (alle skal drikke), som kan redde dig, da der starter en ny runde, hvis du ruller den her"
+          : "The Roll of Cheers (everyone must drink), which can also save you as rolling this will start a new round"}
+      </Box>
+      <RollWithName roll={32} color={colors.blueAccent[100]} sideLength={12} />
+      <Box p={1} />
+
+      <Box display="flex" justifyContent="center">
+        {isDanish
+          ? 'Det højeste slag er "meyer"'
+          : 'The highest roll is "meyer"'}
+      </Box>
+      <RollWithName roll={21} color={colors.blueAccent[100]} sideLength={12} />
+      <Box p={1} />
+
+      <Box display="flex" justifyContent="center">
+        {isDanish
+          ? 'Det næsthøjeste slag er "lille-meyer"'
+          : 'The second highest roll is "meyer"'}
+      </Box>
+      <RollWithName roll={31} color={colors.blueAccent[100]} sideLength={12} />
+      <Box p={1} />
+      {/* PAIRS */}
+      <Box display="flex" justifyContent="center">
+        {isDanish
+          ? "Parrene er de højeste ikke-speciele slag"
+          : "The pairs are the highest non-special rolls"}
+      </Box>
+      <Box display="flex" justifyContent="center">
+        <RollWithName
+          roll={66}
+          color={colors.blueAccent[100]}
+          sideLength={12}
+        />
+
+        <Box marginLeft="3px" />
+        <RollWithName
+          roll={55}
+          color={colors.blueAccent[100]}
+          sideLength={12}
+        />
+
+        <Box marginLeft="3px" />
+        <RollWithName
+          roll={44}
+          color={colors.blueAccent[100]}
+          sideLength={12}
+        />
+
+        <Box marginLeft="3px" />
+        <RollWithName
+          roll={33}
+          color={colors.blueAccent[100]}
+          sideLength={12}
+        />
+
+        <Box marginLeft="3px" />
+        <RollWithName
+          roll={22}
+          color={colors.blueAccent[100]}
+          sideLength={12}
+        />
+
+        <Box marginLeft="3px" />
+        <RollWithName
+          roll={11}
+          color={colors.blueAccent[100]}
+          sideLength={12}
+        />
+      </Box>
+      <Box p={1} />
+
+      {/* BORING ROLLS */}
+      <Box display="flex" justifyContent="center">
+        {isDanish
+          ? "Og til sidst de lave, almindelige slag"
+          : "And finally the low, common rolls"}
+      </Box>
+      <Box display="flex" justifyContent="center">
+        {/* 60s */}
+        <RollWithName
+          roll={65}
+          color={colors.blueAccent[100]}
+          sideLength={12}
+        />
+
+        <Box marginLeft="3px" />
+        <RollWithName
+          roll={64}
+          color={colors.blueAccent[100]}
+          sideLength={12}
+        />
+
+        <Box marginLeft="3px" />
+        <RollWithName
+          roll={63}
+          color={colors.blueAccent[100]}
+          sideLength={12}
+        />
+
+        <Box marginLeft="3px" />
+        <RollWithName
+          roll={62}
+          color={colors.blueAccent[100]}
+          sideLength={12}
+        />
+
+        <Box marginLeft="3px" />
+        <RollWithName
+          roll={61}
+          color={colors.blueAccent[100]}
+          sideLength={12}
+        />
+
+        {/* 50s */}
+        <Box marginLeft="3px" />
+        <RollWithName
+          roll={54}
+          color={colors.blueAccent[100]}
+          sideLength={12}
+        />
+
+        <Box marginLeft="3px" />
+        <RollWithName
+          roll={53}
+          color={colors.blueAccent[100]}
+          sideLength={12}
+        />
+
+        <Box marginLeft="3px" />
+        <RollWithName
+          roll={52}
+          color={colors.blueAccent[100]}
+          sideLength={12}
+        />
+
+        <Box marginLeft="3px" />
+        <RollWithName
+          roll={51}
+          color={colors.blueAccent[100]}
+          sideLength={12}
+        />
+
+        {/* 40s */}
+        <Box marginLeft="3px" />
+        <RollWithName
+          roll={43}
+          color={colors.blueAccent[100]}
+          sideLength={12}
+        />
+
+        <Box marginLeft="3px" />
+        <RollWithName
+          roll={42}
+          color={colors.blueAccent[100]}
+          sideLength={12}
+        />
+
+        <Box marginLeft="3px" />
+        <RollWithName
+          roll={41}
+          color={colors.blueAccent[100]}
+          sideLength={12}
+        />
+      </Box>
+    </Box>
+  );
+};
--- Frontend/src/scenes/Rules/Rules.tsx
+++ Frontend/src/scenes/Rules/Rules.tsx
@@ -5,6 +5,8 @@ import { Outlet } from "react-router-dom";
 import { Dice } from "../../components/icons/DiceIcons";
 import { getRoll, Roll, RollWithName } from "../../utils/diceUtils";
 import { useState } from "react";
+import { HEADING, HOOK, INTRODUCTION } from "./TextSections";
+import { POSSIBLEROLLS } from "./PossibleRoles";
 
 interface Props {
   isDanish: boolean;
@@ -18,228 +20,15 @@ const Rules = ({ isDanish }: Props) => {
   return (
     <Box display="flex" flexBasis="100%" flexDirection="column" overflow="auto">
       {/* HEADING */}
-      <Box display="flex" justifyContent="center" p={2}>
-        <Box display="flex" justifyContent="center">
-          <Typography
-            variant="h1"
-            color={colors.blueAccent[100]}
-            fontWeight="bold"
-            children={isDanish ? "Regler" : "Rules"}
-          />
-        </Box>
-      </Box>
+      <HEADING isDanish={isDanish} />
       {/* HOOK */}
-      <Box display="flex" justifyContent="center" p={2}>
-        <Box display="flex" justifyContent="center">
-          <Typography
-            variant="h5"
-            color={colors.primary[100]}
-            fontWeight="bold"
-            children={
-              isDanish
-                ? "Meyer er et terningespil, hvori der indgår bluf"
-                : "Meyer is a dice game about bluffing"
-            }
-          />
-        </Box>
-      </Box>
+      <HOOK isDanish={isDanish} />
+      {/* INTRODUCTION */}
+      <INTRODUCTION isDanish={isDanish} />
       {/* POSSIBLE ROLLS */}
-      <Box
-        display="flex"
-        justifyContent="center"
-        p={2}
-        flexDirection="column"
-        flexBasis="100%"
-      >
-        <Box display="flex" justifyContent="center">
-          {isDanish
-            ? "Her er alle de slag, man kan rulle, fra højest til lavest:"
-            : "Here are all the possible rolls sorted  from highest to lowest:"}
-        </Box>
-        <Box p={1} />
+      <POSSIBLEROLLS isDanish={isDanish} />
 
-        {/* SPECIAL */}
-        <Box display="flex" justifyContent="center">
-          {isDanish
-            ? "Fællesskålsslaget (alle skal drikke), som kan redde dig, da der starter en ny runde, hvis du ruller den her"
-            : "The Roll of Cheers (everyone must drink), which can also save you as rolling this will start a new round"}
-        </Box>
-        <RollWithName
-          roll={32}
-          color={colors.blueAccent[100]}
-          sideLength={12}
-        />
-        <Box p={1} />
-
-        <Box display="flex" justifyContent="center">
-          {isDanish
-            ? 'Det højeste slag er "meyer"'
-            : 'The highest roll is "meyer"'}
-        </Box>
-        <RollWithName
-          roll={21}
-          color={colors.blueAccent[100]}
-          sideLength={12}
-        />
-        <Box p={1} />
-
-        <Box display="flex" justifyContent="center">
-          {isDanish
-            ? 'Det næsthøjeste slag er "lille-meyer"'
-            : 'The second highest roll is "meyer"'}
-        </Box>
-        <RollWithName
-          roll={31}
-          color={colors.blueAccent[100]}
-          sideLength={12}
-        />
-        <Box p={1} />
-
-        {/* PAIRS */}
-        <Box display="flex" justifyContent="center">
-          {isDanish
-            ? "Parrene er de højeste ikke-speciele slag"
-            : "The pairs are the highest non-special rolls"}
-        </Box>
-        <Box display="flex" justifyContent="center">
-          <RollWithName
-            roll={66}
-            color={colors.blueAccent[100]}
-            sideLength={12}
-          />
-
-          <Box marginLeft="3px" />
-          <RollWithName
-            roll={55}
-            color={colors.blueAccent[100]}
-            sideLength={12}
-          />
-
-          <Box marginLeft="3px" />
-          <RollWithName
-            roll={44}
-            color={colors.blueAccent[100]}
-            sideLength={12}
-          />
-
-          <Box marginLeft="3px" />
-          <RollWithName
-            roll={33}
-            color={colors.blueAccent[100]}
-            sideLength={12}
-          />
-
-          <Box marginLeft="3px" />
-          <RollWithName
-            roll={22}
-            color={colors.blueAccent[100]}
-            sideLength={12}
-          />
-
-          <Box marginLeft="3px" />
-          <RollWithName
-            roll={11}
-            color={colors.blueAccent[100]}
-            sideLength={12}
-          />
-        </Box>
-        <Box p={1} />
-
-        {/* BORING ROLLS */}
-        <Box display="flex" justifyContent="center">
-          {isDanish
-            ? "Og til sidst de lave, almindelige slag"
-            : "And finally the low, common rolls"}
-        </Box>
-        <Box display="flex" justifyContent="center">
-          {/* 60s */}
-          <RollWithName
-            roll={65}
-            color={colors.blueAccent[100]}
-            sideLength={12}
-          />
-
-          <Box marginLeft="3px" />
-          <RollWithName
-            roll={64}
-            color={colors.blueAccent[100]}
-            sideLength={12}
-          />
-
-          <Box marginLeft="3px" />
-          <RollWithName
-            roll={63}
-            color={colors.blueAccent[100]}
-            sideLength={12}
-          />
-
-          <Box marginLeft="3px" />
-          <RollWithName
-            roll={62}
-            color={colors.blueAccent[100]}
-            sideLength={12}
-          />
-
-          <Box marginLeft="3px" />
-          <RollWithName
-            roll={61}
-            color={colors.blueAccent[100]}
-            sideLength={12}
-          />
-
-          {/* 50s */}
-          <Box marginLeft="3px" />
-          <RollWithName
-            roll={54}
-            color={colors.blueAccent[100]}
-            sideLength={12}
-          />
-
-          <Box marginLeft="3px" />
-          <RollWithName
-            roll={53}
-            color={colors.blueAccent[100]}
-            sideLength={12}
-          />
-
-          <Box marginLeft="3px" />
-          <RollWithName
-            roll={52}
-            color={colors.blueAccent[100]}
-            sideLength={12}
-          />
-
-          <Box marginLeft="3px" />
-          <RollWithName
-            roll={51}
-            color={colors.blueAccent[100]}
-            sideLength={12}
-          />
-
-          {/* 40s */}
-          <Box marginLeft="3px" />
-          <RollWithName
-            roll={43}
-            color={colors.blueAccent[100]}
-            sideLength={12}
-          />
-
-          <Box marginLeft="3px" />
-          <RollWithName
-            roll={42}
-            color={colors.blueAccent[100]}
-            sideLength={12}
-          />
-
-          <Box marginLeft="3px" />
-          <RollWithName
-            roll={41}
-            color={colors.blueAccent[100]}
-            sideLength={12}
-          />
-        </Box>
-      </Box>
-      {/* Random Roll */}
+      {/* RANDOM ROLL */}
       <Box display="flex" flexDirection="column">
         <Box display="flex" justifyContent="center" flexBasis="100%">
           {roll == 0 && "Click below for random roll!"}
