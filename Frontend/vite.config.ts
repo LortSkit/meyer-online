b3d832e3d8e01a0293ec23f2d998cc23f5b5fa40
LortSkit <alexander@laukamp.dk>
2025-02-08 17:52:42 +0100

    supress warnings when building

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -6,6 +6,17 @@ config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 100,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
   plugins: [
     react(),
     mkcert({
